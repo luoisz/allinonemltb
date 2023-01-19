e0926b62a2cd303c7ab0fe75c6961486e5e14f5d
luoisz <86519689+luoisz@users.noreply.github.com>
2023-01-19 23:19:15 +0630

    Update update.py

--- update.py
+++ update.py
@@ -12,7 +12,7 @@ basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     handlers=[FileHandler('log.txt'), StreamHandler()],
                     level=INFO)
                     
-CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL')
+CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL', '')
 try:
     if len(CONFIG_FILE_URL) == 0:
         raise TypeError
