c86e7b027510811940cf425d5aef7abc940c7c82
luoisz <86519689+luoisz@users.noreply.github.com>
2023-01-20 16:10:11 +0630

    Update update.py

--- update.py
+++ update.py
@@ -1,9 +1,8 @@
 from logging import FileHandler, StreamHandler, INFO, basicConfig, error as log_error, info as log_info
-from os import path as ospath, environ, execl as osexecl
+from os import path as ospath, environ
 from subprocess import run as srun
 from requests import get as rget
 from dotenv import load_dotenv
-from sys import executable
 
 if ospath.exists('log.txt'):
     with open('log.txt', 'r+') as f:
@@ -15,33 +14,47 @@ basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
 
 load_dotenv('config.env', override=True)
 
-UPSTREAM_REPO = environ.get('UPSTREAM_REPO')
-UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH')
-try:
-    if len(UPSTREAM_REPO) == 0:
-       raise TypeError
-except:
-    UPSTREAM_REPO = None
-try:
-    if len(UPSTREAM_BRANCH) == 0:
-       raise TypeError
-except:
+BOT_TOKEN = environ.get('BOT_TOKEN', '')
+if len(BOT_TOKEN) == 0:
+    log_error("BOT_TOKEN variable is missing! Exiting now")
+    exit(1)
+
+bot_id = int(BOT_TOKEN.split(':', 1)[0])
+
+DATABASE_URL = environ.get('DATABASE_URL', '')
+if len(DATABASE_URL) == 0:
+    DATABASE_URL = None
+
+if DATABASE_URL is not None:
+    conn = MongoClient(DATABASE_URL)
+    db = conn.mltb
+    if config_dict := db.settings.config.find_one({'_id': bot_id}):  #retrun config dict (all env vars)
+        environ['UPSTREAM_REPO'] = config_dict['UPSTREAM_REPO']
+        environ['UPSTREAM_BRANCH'] = config_dict['UPSTREAM_BRANCH']
+    conn.close()
+
+UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
+if len(UPSTREAM_REPO) == 0:
+   UPSTREAM_REPO = None
+
+UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
+if len(UPSTREAM_BRANCH) == 0:
     UPSTREAM_BRANCH = 'master'
 
-if ospath.exists('.git'):
-    srun(["rm", "-rf", ".git"])
+if UPSTREAM_REPO is not None:
+    if ospath.exists('.git'):
+        srun(["rm", "-rf", ".git"])
 
-update = srun([f"git init -q \
-                 && git config --global user.email doc.adhikari@gmail.com \
-                 && git config --global user.name Karan \
-                 && git add . \
-                 && git commit -sm update -q \
-                 && git remote add origin {UPSTREAM_REPO} \
-                 && git fetch origin -q \
-                 && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
+    update = srun([f"git init -q \
+                     && git config --global user.email e.anastayyar@gmail.com \
+                     && git config --global user.name mltb \
+                     && git add . \
+                     && git commit -sm update -q \
+                     && git remote add origin {UPSTREAM_REPO} \
+                     && git fetch origin -q \
+                     && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
 
-UPSTREAM_REPO_URL = (UPSTREAM_REPO[:8] if UPSTREAM_REPO[:8] and UPSTREAM_REPO[:8].endswith('/') else UPSTREAM_REPO[:7]) + UPSTREAM_REPO.split('@')[1] if '@github.com' in UPSTREAM_REPO else UPSTREAM_REPO     
-if update.returncode == 0:
-    log_info(f'Successfully updated with latest commit from {UPSTREAM_REPO_URL}')
-else:
-    log_error(f'Something went wrong while updating, check {UPSTREAM_REPO_URL} if valid or not!')
+    if update.returncode == 0:
+        log_info('Successfully updated with latest commit from UPSTREAM_REPO')
+    else:
+        log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
